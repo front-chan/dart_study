20876cbfb39410aa14be155cc2e4d0d0a972b607
front-chan <chan.jor.zz@gmail.com>
2023-04-06 17:17:19 +0900

    Dart Study _ late variables

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 void main() {
-  final name = 'chan';
-  // 굳이 할 필요 없지만 구체적으로 해주고 싶다면 명시적 타입을 지정할 수 있다
-  // final String name = 'chan';
-  // name = 'chanchan'; // 에러발생
+  late final name;
+  // print(name); // 데이터가 없기 때문에 에러 발생
+
+  // 정확하게 해주려면 타입 지정 가능
+  // late final String name;
+
+  // api를 받아 변수에 넣어주기
+  name = 'chan';
 }
